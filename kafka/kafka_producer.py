dec8045d04f5c4df4dbe170c36a30522200b2b47
RicadoKim <may3210@naver.com>
2023-09-07 22:35:18 +0900

    Completed MongoDB Docker setup and implemented data transmission from Spark to MongoDB

--- kafka/kafka_producer.py
+++ kafka/kafka_producer.py
@@ -2,7 +2,7 @@
 #2. process 정보 관련 토픽을 생성한다.
 #3. 토픽에 adb로 읽은 정보를 보내게 된다.
 import subprocess
-import time
+import re
 
 from kafka import KafkaProducer
 from json import dumps
@@ -57,44 +57,39 @@ process_producer = KafkaProducer(
     value_serializer = lambda x: dumps(x).encode('utf-8')
 )
 
+def parse_android_top_output(output_str):
+    # 각 상태에 대한 정규 표현식 패턴을 정의합니다.
+    patterns = {
+        "Tasks": r"Tasks:\s*(\d+)\s*total",
+        "Running": r"(\d+)\s*running",
+        "Sleeping": r"(\d+)\s*sleeping",
+        "Stopped": r"(\d+)\s*stopped",
+        "Zombie": r"(\d+)\s*zombie"
+    }
+    
+    # 결과를 저장할 딕셔너리를 생성합니다.
+    result = {}
+
+    # 각 패턴에 대해 문자열에서 값을 찾아 결과 딕셔너리에 저장합니다.
+    for key, pattern in patterns.items():
+        match = re.search(pattern, output_str)
+        if match:
+            result[key] = int(match.group(1))
+        else:
+            result[key] = None
+
+    return result
+
 def extract_top():
-    section = ""
-    last_log = ""
     adb_process = subprocess.Popen(['adb', 'shell', 'top', '-m', '10'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     while True:
         log = adb_process.stdout.readline().decode().strip()
-        if log != last_log:
-            last_section = section
-            prefix = log
-            print(log)
-            if "Task" in prefix :
-                section = "Task"
-                print(log)
-                task_producer.send(
-                    'Android_Task', 
-                        value = {
-                            "value" : log
-                        }
-                    )
-            elif "Mem" in prefix:
-                section = "Memory"
-                task_producer.send('Android_Mem', {
-                    section : log
-                })
-            elif "Swap" in prefix:
-                section = "Swap"
-                task_producer.send('Android_Swap', {
-                    section : log
-                })
-            elif "PID" in prefix :
-                section = "Process"
-            elif last_section == "Process" :
-                task_producer.send('Android_Process', {
-                    section : log
-                })
-            else :
-                pass
-            last_log = log
+        if "Task" in log :
+            parsed_data = parse_android_top_output(log)
+            task_producer.send(
+                'Android_Task', 
+                    value=parsed_data
+                )
 
 if __name__ == "__main__":
     extract_top()
--- spark/spark_consumer.py
+++ spark/spark_consumer.py
@@ -1,6 +1,16 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import from_json, col
-from pyspark.sql.types import StructType, StructField, StringType
+from pyspark.sql.functions import from_json, col, expr
+from pyspark.sql.types import StructType, StructField, IntegerType
+from pymongo import MongoClient
+
+def write_to_mongo(df, epoch_id):
+    mongo_client = MongoClient('mongodb://root:1234@mongodb:27017/')  # Docker Compose 서비스 이름과 포트를 사용하여 MongoDB에 연결합니다.
+    db = mongo_client.AndroidLogDataMart  # Database를 선택합니다.
+    collection = db.AndroidResourceMonitoring  # Collection을 선택합니다.
+    
+    data = df.collect()  # Spark DataFrame을 Python 리스트로 변환합니다.
+    for row in data:
+        collection.insert_one(row.asDict())  # 각 로우를 MongoDB에 삽입합니다.
 
 # SparkSession을 생성합니다.
 spark = SparkSession.builder \
@@ -17,15 +27,29 @@ kafka_params = {
 raw_stream = spark.readStream.format("kafka").options(**kafka_params).load()
 
 # 읽어온 데이터의 값 부분을 JSON 형식으로 변환합니다.
-value_schema = StructType([StructField("value", StringType(), True)])
+value_schema = StructType([
+    StructField("Tasks", IntegerType(), True),
+    StructField("Running", IntegerType(), True),
+    StructField("Sleeping", IntegerType(), True),
+    StructField("Stopped", IntegerType(), True),
+    StructField("Zombie", IntegerType(), True)
+])
 json_stream = raw_stream.selectExpr("CAST(value AS STRING) AS json") \
     .select(from_json("json", value_schema).alias("data")) \
     .select("data.*")
 
+
+
+# 원하는 로직을 적용하여 데이터를 변환합니다.
+transformed_stream = json_stream.withColumn("Task", col("Tasks") - 1) \
+    .withColumn("Running", col("Running") + 1) \
+    .withColumn("Sleeping", col("Sleeping") - 1) \
+    .drop("Tasks")
+
 # 데이터를 출력하거나 원하는 작업을 수행합니다.
-query = json_stream.writeStream \
+query = transformed_stream.writeStream \
     .outputMode("append") \
-    .format("console") \
+    .foreachBatch(write_to_mongo) \
     .start()
 
 # 스트리밍 작업을 시작합니다.
